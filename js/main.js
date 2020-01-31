41f3b7a13709d063dae9f7b00430de7f08f7356f
Maxim <m.radik25@gmail.com>
2020-01-31 22:49:37 +0300

    add big picture block

--- js/main.js
+++ js/main.js
@@ -8,6 +8,14 @@ var PHOTOS_NUMBER = 25;
 var picturesContainer = document.querySelector('.js-pictures-container');
 var userPicture = document.querySelector('#picture');
 var userPictureItem = userPicture.content.querySelector('.js-user-picture');
+var pictureBlock = document.querySelector('.js-picture');
+var pictureImg = document.querySelector('.js-picture-img');
+var pictureLikes = document.querySelector('.js-picture-likes');
+var pictureCommentsCount = document.querySelector('.js-comments-count');
+var pictureCommentsCountBlock = document.querySelector('.js-comments-count-block');
+var pictureCommentsBlock = document.querySelector('.js-social-comments');
+var pictureCommentsLoader = document.querySelector('.js-comments-loader');
+var pictureDesc = document.querySelector('.js-picture-description');
 
 var photos = [];
 
@@ -72,6 +80,8 @@ function addElements(elements) {
   for (var i = 0; i < PHOTOS_NUMBER; i++) {
     elements.appendChild(renderPhoto(photos[i]));
   }
+
+  return elements;
 }
 
 photos = createPhotos(PHOTOS_NUMBER);
@@ -80,3 +90,32 @@ addElements(fragment);
 
 picturesContainer.appendChild(fragment);
 
+pictureBlock.classList.remove('hidden');
+pictureCommentsCountBlock.classList.add('hidden');
+pictureCommentsLoader.classList.add('hidden');
+document.body.classList.add('modal-open');
+
+function renderComment(userComment) {
+  for (var i = 0; i < userComment.comments.length; i++) {
+    var commentItem = document.querySelector('.js-social-comment').cloneNode(true);
+    var commentImg = commentItem.querySelector('.js-social-comment-picture');
+    var commentText = commentItem.querySelector('.js-social-comment-text');
+
+    commentImg.src = userComment.comments[i].avatar;
+    commentImg.alt = userComment.comments[i].name;
+    commentText.textContent = userComment.comments[i].message;
+
+    pictureCommentsBlock.appendChild(commentItem);
+  }
+}
+
+function renderPictureBlock(pictureInfo) {
+  pictureImg.src = pictureInfo.url;
+  pictureLikes.textContent = pictureInfo.likes;
+  pictureCommentsCount.textContent = pictureInfo.comments.length;
+  pictureDesc.textContent = pictureInfo.description;
+
+  renderComment(pictureInfo);
+}
+
+renderPictureBlock(photos[0]);
